7bc817bef2fbe6500ada111af52cb3818402a7e1
Dragoș Efrem <101568589+dragosefrem@users.noreply.github.com>
2022-06-06 14:12:50 +0300

    Project structure has been created

---
+++ src/main/java/module-info.java
@@ -0,0 +1,13 @@
+module edu.poli.efrem.microbenchmark {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens edu.poli.efrem.microbenchmark to javafx.fxml;
+    exports edu.poli.efrem.microbenchmark;
+}
